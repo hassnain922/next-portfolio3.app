d49d3dec50b98dd145649a0aac183587a43e0b7f
hassnain922 <hassnain121@gmail.com>
2023-07-10 07:23:41 +0500

    commit

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <div className=' flex flex-col items-center justify-center mx-4 py-2 h-full bg-purple-600 text-white'>
-       <p className='text-center'> copyright 2023 | create with passion by futuristic Engineer </p>
+       <p className='text-center px-4'> copyright 2023 | create with passion by futuristic Engineer </p>
     </div>
   )
 }
